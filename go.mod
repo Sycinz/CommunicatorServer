2ca9bc269ad082296d2c1a56098073b2f1fe0f49
NintySwinty <emil.panecki@gmail.com>
2024-05-05 21:45:24 +0200

    Server update, now works chat and list of users on server

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module BetterCommunicatorServer
 
 go 1.21.8
 
-require github.com/google/uuid v1.6.0 // indirect
+require github.com/google/uuid v1.6.0
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	gorm.io/driver/sqlite v1.5.5 // indirect
+	gorm.io/gorm v1.25.10 // indirect
+)
--- main.go
+++ main.go
@@ -1,166 +1,254 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"time"
-	// "strings"
-	"github.com/google/uuid"
-	"encoding/json"
+    "encoding/json"
+    "fmt"
+    "net"
+    "time"
+    "github.com/google/uuid"
+	"gorm.io/gorm"
+    "gorm.io/driver/sqlite"
 )
 
 type User struct {
-	Nick    string `json:"Nick"`
-	Image   string `json:"Image"`
-	UUID    string
-	Connection net.Conn
-	// IPv4    string
+    Nick       string `json:"Nick"`
+    Image      string `json:"Image"`
+    UUID       string
+    Connection net.Conn
+	Permission string `json:"Permission"`
+	Rank 	   string `json:"Rank"`
 }
 
 type Message struct {
-	Nick    string `json:"Nick"`
-	Message string `json:"Message"`
-	Date	string `json:"Date"`
+    Nick    string `json:"Nick"`
+    Message string `json:"Message"`
+    Date    string `json:"Date"`
+}
+
+type UsersList struct {
+    Users []User `json:"Users"`
+}
+
+type Type int
+
+const (
+	Chat Type = 1
+	Voice Type = 2
+)
+
+type Channel struct {
+	Users []User
+	MaximumUsers int
+	ChannelName string
+	ChannelDescription string
+	ChannelPassword string
+	ChannelType Type
+	CreationTime time.Time
 }
 
 var users []User
 
 func handleConnection(conn net.Conn) {
-	defer conn.Close()
+    defer conn.Close()
 
-	buffer := make([]byte, 2000)
+    buffer := make([]byte, 2000)
 
-	// Odbierz JSON od klienta który ma tylko wartość "Nick"
-	n, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Błąd odczytu danych:", err)
-		return
-	}
+    n, err := conn.Read(buffer)
+    if err != nil {
+        fmt.Println("Błąd odczytu danych:", err)
+        return
+    }
 
-	user := User{
-		Image: "Empty",
-		UUID: uuid.New().String(),
-		Connection: conn,
-	}
+    user := User{
+        Image:      "Empty",
+        UUID:       uuid.New().String(),
+        Connection: conn,
+    }
 
-	// Przetwarzanie JSON
-	nickJSON := string(buffer[:n])
-	json.Unmarshal([]byte(nickJSON), &user)
-	fmt.Printf("Nick od klienta: %s\n", user.Nick)
+    nickJSON := string(buffer[:n])
+    json.Unmarshal([]byte(nickJSON), &user)
+    fmt.Printf("Nick od klienta: %s\n", user.Nick)
 
-	fmt.Println("Nowy użytkownik:", user.Nick)
+    fmt.Println("Nowy użytkownik:", user.Nick)
 
-	// Dodawanie użytkownika do tablicy
-	users = append(users, user)
+    users = append(users, user)
 
-	var usersList string = ""
+    sendUsersListToAll()
 
-	for x, otherUser := range users {
-		usersList += "{\"Nick\": \"" + otherUser.Nick + "\", \"Image\": \"" + otherUser.Image + "\"}"
-		if x < len(users) - 1 {
-			usersList += ", "
-		}
-	}
+    buffer = make([]byte, 2000)
+    for {
+        n, err := conn.Read(buffer)
+        if err != nil {
+            fmt.Println("Błąd odczytu danych:", err)
+            buffer = nil
+            break
+        }
 
-	var usersJSON string = "{ \"Users\": [" + usersList + "] }"
+        message := Message{}
 
-	_, err = conn.Write([]byte(usersJSON))
+        messageJSON := string(buffer[:n])
+        json.Unmarshal([]byte(messageJSON), &message)
+        fmt.Printf("Wiadomość od %s: %s\n", user.Nick, message.Message)
 
-	// Odbieranie wiadomości od klienta
-	buffer = make([]byte, 2000)
-	for {
-		n, err := conn.Read(buffer)
-		if err != nil {
-			fmt.Println("Błąd odczytu danych:", err)
-			buffer = nil
-			break
-		}
+        message.Nick = user.Nick
 
-		message := Message{};
+        currentTime := time.Now()
 
-		// Przetwarzanie wiadomości
-		messageJSON := string(buffer[:n])
-		json.Unmarshal([]byte(messageJSON), &message)
-		fmt.Printf("Wiadomość od %s: %s\n", user.Nick, message.Message)
+        message.Date = "" + currentTime.Format("2006-01-02 15:04:05")
 
-		message.Nick = user.Nick
+        sendMessageToAll(message)
 
-		currentTime := time.Now()
+    }
 
-		message.Date = "" + currentTime.Format("2006-01-02 15:04:05")
+    removeUser(user)
+    fmt.Println("Użytkownik rozłączony:", user.Nick)
 
-		// Wysyłanie wiadomości do innych użytkowników
-		for _, _ = range users {
-			// if otherUser.UUID != user.UUID {
-				json, err := json.Marshal(message)
-				if err != nil {
-					fmt.Println("Błąd przetwarzania danych:", err)
-					buffer = nil
-					break
-				}
+    sendUsersListToAll()
 
-				_, err = conn.Write([]byte(json))
-				if err != nil {
-					fmt.Println("Błąd wysyłania danych:", err)
-					buffer = nil
-					break
-				}
-			// }
-		}
-	}
+    message := Message{}
+    message.Nick = "Server"
+    message.Message = user.Nick + " rozłączył się"
+    currentTime := time.Now()
+    message.Date = "" + currentTime.Format("2006-01-02 15:04:05")
 
-	// Usuwanie użytkownika po rozłączeniu
-	for i, existingUser := range users {
-		if existingUser.UUID == user.UUID {
-			// Usuń użytkownika z listy
-			users = append(users[:i], users[i+1:]...)
-			break
-		}
-	}
-	fmt.Println("Użytkownik rozłączony:", user.Nick)
+    json, err := json.Marshal(message)
+    if err != nil {
+        fmt.Println("Błąd przetwarzania danych:", err)
+    }
 
-	message := Message{};
-	message.Nick = "Server"
-	message.Message = user.Nick + " rozłączył się"
-	currentTime := time.Now()
-	message.Date = "" + currentTime.Format("2006-01-02 15:04:05")
+    _, err = conn.Write([]byte(json))
+    if err != nil {
+        fmt.Println("Błąd wysyłania danych:", err)
+    }
 
-	json, err := json.Marshal(message)
-	if err != nil {
-		fmt.Println("Błąd przetwarzania danych:", err)
-	}
-
-	_, err = conn.Write([]byte(json))
-	if err != nil {
-		fmt.Println("Błąd wysyłania danych:", err)
-	}
-
-	conn.Close()
+    conn.Close()
 
 }
 
 func main() {
-	// Uruchamianie serwera na porcie 8080
-	listener, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		fmt.Println("Błąd uruchamiania serwera:", err)
-		return
-	}
-	defer listener.Close()
+	// Check if server.db file exists, if not create it
+	db, err := gorm.Open(sqlite.Open("server.db"), &gorm.Config{})
+  	if err != nil {
+    	panic("failed to connect database")
+  	}
 
-	fmt.Println("Serwer czatu TCP uruchomiony na porcie 8080")
+	// Create tables if they don't exist
+	db.AutoMigrate(&Channel{})
 
-	// Akceptowanie połączeń od klientów
-	for {
-		conn, err := listener.Accept()
+	// TCP server
+	go func() {
+		listener, err := net.Listen("tcp", ":8080")
 		if err != nil {
-			fmt.Println("Błąd akceptowania połączenia:", err)
-			continue
-		} else {
-			fmt.Println("Nowe połączenie:", conn.RemoteAddr())
+			fmt.Println("Błąd uruchamiania serwera TCP:", err)
+			return
+		}
+		defer listener.Close()
+
+		fmt.Println("Serwer czatu TCP uruchomiony na porcie 8080")
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Błąd akceptowania połączenia TCP:", err)
+				continue
+			} else {
+				fmt.Println("Nowe połączenie TCP:", conn.RemoteAddr())
+			}
+
+			go handleConnection(conn)
+		}
+	}()
+
+	// UDP server
+	go func() {
+		udpAddr, err := net.ResolveUDPAddr("udp", ":8081")
+		if err != nil {
+			fmt.Println("Błąd uruchamiania serwera UDP:", err)
+			return
 		}
 
-		// Obsługa połączenia w nowym wątku
-		go handleConnection(conn)
+		udpConn, err := net.ListenUDP("udp", udpAddr)
+		if err != nil {
+			fmt.Println("Błąd uruchamiania serwera UDP:", err)
+			return
+		}
+		defer udpConn.Close()
+
+		fmt.Println("Serwer przesyłania głosu UDP uruchomiony na porcie 8081")
+
+		buffer := make([]byte, 1024)
+
+		for {
+			n, addr, err := udpConn.ReadFromUDP(buffer)
+			if err != nil {
+				fmt.Println("Błąd odczytu danych UDP:", err)
+				continue
+			} else {
+				fmt.Println("Nowe połączenie UDP:", addr)
+			}
+
+			// Handle voice data here
+			// ...
+
+			// Example: Echo back the received data
+			_, err = udpConn.WriteToUDP(buffer[:n], addr)
+			if err != nil {
+				fmt.Println("Błąd wysyłania danych UDP:", err)
+			}
+		}
+	}()
+
+	// Wait indefinitely
+	select {}
+}
+
+func sendUsersListToAll() {
+	userList := make([]User, len(users))
+	for i, user := range users {
+		userList[i] = User{
+			Nick:  user.Nick,
+			Image: user.Image,
+		}
 	}
+
+	usersList := UsersList{
+		Users: userList,
+	}
+
+	usersJSON, err := json.Marshal(usersList)
+	if err != nil {
+		fmt.Println("Error processing data:", err)
+		return
+	}
+
+	for _, user := range users {
+		_, err := user.Connection.Write([]byte(usersJSON))
+		if err != nil {
+			fmt.Println("Error sending data:", err)
+		}
+	}
+}
+
+func sendMessageToAll(message Message) {
+    json, err := json.Marshal(message)
+    if err != nil {
+        fmt.Println("Błąd przetwarzania danych:", err)
+        return
+    }
+
+    for _, user := range users {
+        _, err := user.Connection.Write([]byte(json))
+        if err != nil {
+            fmt.Println("Błąd wysyłania danych:", err)
+        }
+    }
+}
+
+func removeUser(user User) {
+    for i, existingUser := range users {
+        if existingUser.UUID == user.UUID {
+            users = append(users[:i], users[i+1:]...)
+            break
+        }
+    }
 }
