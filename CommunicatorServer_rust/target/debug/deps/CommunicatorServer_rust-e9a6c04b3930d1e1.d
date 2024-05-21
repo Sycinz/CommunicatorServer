e9b93eeb553ccc8a436406176e67acad6c16c617
Mateusz <60658488+Sycinz@users.noreply.github.com>
2024-05-21 10:15:10 +0200

    Delete CommunicatorServer_rust directory

--- CommunicatorServer_rust/target/debug/deps/CommunicatorServer_rust-e9a6c04b3930d1e1.d
+++
@@ -1,5 +0,0 @@
-/home/mati/Code/Rust/CommunicatorServer/CommunicatorServer_rust/target/debug/deps/libCommunicatorServer_rust-e9a6c04b3930d1e1.rmeta: src/main.rs
-
-/home/mati/Code/Rust/CommunicatorServer/CommunicatorServer_rust/target/debug/deps/CommunicatorServer_rust-e9a6c04b3930d1e1.d: src/main.rs
-
-src/main.rs:
--- CommunicatorServer_rust/target/debug/deps/signal_hook_registry-fcabb27e3fe2e1b2.d
+++
@@ -1,6 +0,0 @@
-/home/mati/Code/Rust/CommunicatorServer/CommunicatorServer_rust/target/debug/deps/libsignal_hook_registry-fcabb27e3fe2e1b2.rmeta: /home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.2/src/lib.rs /home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.2/src/half_lock.rs
-
-/home/mati/Code/Rust/CommunicatorServer/CommunicatorServer_rust/target/debug/deps/signal_hook_registry-fcabb27e3fe2e1b2.d: /home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.2/src/lib.rs /home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.2/src/half_lock.rs
-
-/home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.2/src/lib.rs:
-/home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.2/src/half_lock.rs:
