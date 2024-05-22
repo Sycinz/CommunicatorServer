a557c8c24314b209b3957192f0265f926be82990
Sycinz <gronbosof@gmail.com>
2024-05-22 19:36:22 +0200

    RustRover check

--- rust_server/target/debug/deps/actix_macros-f44030c6beee5a9f.d
+++ rust_server/target/debug/deps/actix_macros-f44030c6beee5a9f.d
@@ -1,5 +1,5 @@
-/home/mati/Code/Rust/CommunicatorServer/CommunicatorServer_rust/target/debug/deps/libactix_macros-f44030c6beee5a9f.so: /home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-macros-0.2.4/src/lib.rs
+/home/mati/RustroverProjects/CommunicatorServer/rust_server/target/debug/deps/libactix_macros-f44030c6beee5a9f.so: /home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-macros-0.2.4/src/lib.rs
 
-/home/mati/Code/Rust/CommunicatorServer/CommunicatorServer_rust/target/debug/deps/actix_macros-f44030c6beee5a9f.d: /home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-macros-0.2.4/src/lib.rs
+/home/mati/RustroverProjects/CommunicatorServer/rust_server/target/debug/deps/actix_macros-f44030c6beee5a9f.d: /home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-macros-0.2.4/src/lib.rs
 
 /home/mati/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-macros-0.2.4/src/lib.rs:
